1ed7ab3ada3859cd5bd55fba31814ec469f8c899
MicSobSih <Michael.SobreiradeFreitasSihle@rub.de>
2025-10-24 21:20:37 +0200

    created first simple model answering

---
+++ rag_pipeline.py
@@ -0,0 +1,15 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts.chat import HumanMessagePromptTemplate, SystemMessagePromptTemplate, ChatPromptTemplate
+
+prompt = ChatPromptTemplate([
+    SystemMessagePromptTemplate.from_template("You are a helpful assistant. Answer the question as best you can."),
+    HumanMessagePromptTemplate.from_template("{input}"),
+])
+
+llm = ChatOllama(model="llama3")
+
+chain = prompt | llm
+answer = chain.invoke({
+    "input": "What is the capital of France?",
+})
+print(answer.content)
