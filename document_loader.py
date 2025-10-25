ab5f13ace281c96597b50134e430614ede3d266e
MicSobSih <Michael.SobreiradeFreitasSihle@rub.de>
2025-10-25 15:11:36 +0200

    loaded website document

--- document_loader.py
+++ document_loader.py
@@ -1,6 +1,15 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import WebBaseLoader
 
-loader = TextLoader("climate_change.txt")
-documents = loader.load()
-print(documents)
-print(documents[0].page_content)
+txt_loader = TextLoader("climate_change.txt")
+txt_documents = txt_loader.load()
+print(txt_documents)
+print(txt_documents[0].page_content)
+
+web_loader = WebBaseLoader("https://en.wikipedia.org/wiki/COVID-19")
+web_documents = web_loader.load()
+print(web_documents)
+print(web_documents[0].page_content)
+
+# there are many more loaders provided by langchain
+# like e.g. pdf, csv, etc.
