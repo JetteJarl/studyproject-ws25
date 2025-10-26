d43a236b7c9ca2eaf587b8d613ad22f107f002e2
MicSobSih <Michael.SobreiradeFreitasSihle@rub.de>
2025-10-26 20:02:31 +0100

    deleted unused function because embeddings are being created by the database already

--- document_embedding.py
+++ document_embedding.py
@@ -1,13 +1,8 @@
-from typing import Iterable, List
 from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_embeddings_model(model_name: str) -> HuggingFaceEmbeddings:
     embeddings_model = HuggingFaceEmbeddings(model_name=model_name)
     return embeddings_model
-
-def embed_texts(embeddings_model: HuggingFaceEmbeddings, texts: Iterable[str]) -> List[List[float]]:
-    embeddings = embeddings_model.embed_documents(list(texts))
-    return embeddings
 
 # there are many more embeddings provided by huggingface, openai, google, etc.
 # check out the leaderboard of embedding models
