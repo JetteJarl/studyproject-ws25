5dde36007f88113002c00182ef9cb170a7599ced
MicSobSih <Michael.SobreiradeFreitasSihle@rub.de>
2025-10-28 20:48:12 +0100

    created a simple UI

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,4 +1,5 @@
 from langchain_core.documents import Document
+import streamlit as st
 
 from document_loader import load_web_page
 from document_splitter import split_documents
@@ -6,37 +7,24 @@ from embed_model import get_embeddings_model
 from vector_database import save_vectorstore, load_vectorstore, get_retriever
 from llm_model import build_llm, generate_answer
 
-def build_rag_pipeline(
-    *,
-    source: str,
-    embed_model_name: str,
-    persist_directory: str,
-    chunk_size: int = 1000,
-    chunk_overlap: int = 200,
-    retriever_k: int = 3
-):
-    embeddings_model = get_embeddings_model(embed_model_name)
+st.set_page_config(page_title="RAG Pipeline Prototype", page_icon="🤖", layout="wide")
+st.title("Automated Counterstatement Generation against Misinformation via Generative AI")
 
-    vectorstore = load_vectorstore(embeddings_model, persist_directory)
-    if vectorstore is None:
-        docs = load_web_page(source)
-        chunks: list[Document] = split_documents(docs, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-        vectorstore = save_vectorstore(chunks, embeddings_model, persist_directory)
+url = st.text_input(label="Enter a URL to load:", value="https://en.wikipedia.org/wiki/COVID-19")
 
-    retriever = get_retriever(vectorstore, k=retriever_k)
-    return retriever
+embeddings_model = get_embeddings_model("sentence-transformers/all-mpnet-base-v2")
+datastore_name = "chroma_db"
 
-if __name__ == "__main__":
-    retriever = build_rag_pipeline(
-        source="https://en.wikipedia.org/wiki/COVID-19",
-        chunk_size=1000,
-        chunk_overlap=200,
-        embed_model_name="sentence-transformers/all-mpnet-base-v2",
-        retriever_k=3,
-        persist_directory="chroma_db",
-    )
+vectorstore = load_vectorstore(embeddings_model, datastore_name)
+if vectorstore is None:
+    docs = load_web_page(url)
+    chunks: list[Document] = split_documents(docs, chunk_size=1000, chunk_overlap=200)
+    vectorstore = save_vectorstore(chunks, embeddings_model, datastore_name)
 
-    chain = build_llm(model="llama3")
-    question = "Is the vaccine effective?"
-    answer = generate_answer(question=question, retriever=retriever, chain=chain)
-    print(answer)
+retriever = get_retriever(vectorstore, k=3)
+st.success("RAG Pipeline initialized!")
+
+chain = build_llm(model="llama3")
+query = st.text_input(label="Say something: ", value="Is the vaccine effective?")
+answer = generate_answer(question=query, retriever=retriever, chain=chain)
+st.write("Answer: ", answer)
